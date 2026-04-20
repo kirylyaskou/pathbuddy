ada579094766f21354e3d5e61eeb03235a5a5d7d
K.Yaskou <kirylyaskou@gmail.com>
2026-04-20 19:17:21 +0300

    feat(75-01): mount useStartupUpdateCheck in AppProviders
    - Hook invoked in AppProviders body before return (D-06)
    - Silent startup check triggers UpdateDialog auto-open on available update
    - AUTO-01 closed: no manual click to discover updates in prod

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from 'next-themes'
 import { Toaster } from '@/shared/ui/sonner'
 import { UpdateDialog } from '@/widgets/update-dialog'
+import { useStartupUpdateCheck } from './useStartupUpdateCheck'
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
+  useStartupUpdateCheck()
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
       {children}
